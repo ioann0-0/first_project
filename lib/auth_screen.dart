e6477894393ab2ed6c2ef5e3c906194be856142e
Ivan <ivanerachin17@gmail.com>
2021-10-20 23:22:08 +0600

    кнопки

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -12,19 +12,31 @@ class AuthScreen extends StatelessWidget {
       child: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            CupertinoButton.filled(
-              child: Text('Войти'),
-              onPressed: () {},
-            ),
-            CupertinoButton.filled(
-              child: Text('Зарегистрироваться'),
-              onPressed: () {},
-            ),
             CupertinoTextField(placeholder: 'Логин'),
             CupertinoTextField(
               placeholder: 'Пароль',
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: CupertinoButton(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                color: Color(0xFF4631D2),
+                child: Text('Войти'),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(height: 19),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: CupertinoButton(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  color: Color(0xFF4631D2),
+                  child: Text('Зарегистрироваться'),
+                  onPressed: () {},
+                )),
+            SizedBox(height: 32)
           ],
         ),
       ),
